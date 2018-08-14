670794fd1507155d01320c0b565dcd761f2c2d58
Borguuh <abdrahmanadewale@gmail.com>
2018-08-14 12:16:24 +0100

    fetching book data

--- BookFinder App/src/App.js
+++ BookFinder App/src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import {Grid, Col, Row} from 'react-bootstrap';
+import axios from 'axios';
 import Header from './Components/Header';
 import Books from './Components/Books';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      books:[],
+      text:'Harry Potter'
+    }
+  }
+
+  componentWillMount(){
+    this.getBooks();
+  }
+
+  getBooks(){
+    axios.request({
+      method:'get',
+      url:'https://www.googleapis.com/books/v1/volumes?q='+this.state.text
+    }).then((response) => {
+      this.setState({books: response.data.items}, () => {
+        console.log(this.state);
+      });
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,7 +38,7 @@ class App extends Component {
         <Grid>
           <Row>
             <Col xs={12} md={12} lg={12}>
-              <Books />
+              <Books books={this.state.books} />
             </Col>
           </Row>
         </Grid>

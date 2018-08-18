5533ade617bfbe6b8d042339a301f354fe50a7d0
Borguuh <abdrahmanadewale@gmail.com>
2018-08-18 12:21:12 +0100

    simple bar chart

--- D3 Bar Chart/src/Components/BarChart.js
+++ D3 Bar Chart/src/Components/BarChart.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import * as d3 from "d3";
+import ReactFauxDOM from 'react-faux-dom';
 
 class BarChart extends Component {
   constructor(props){
@@ -8,12 +10,37 @@ class BarChart extends Component {
     }
   }
 
+  static defaultProps = {
+    height: 500,
+    width: 500,
+    chartBg:'#f4f4f4',
+    barColor:'steelBlue',
+    barWidth: 40,
+    barOffset: 5
+  }
+
   render() {
-    return (
-      <div>
-        BARCHART
-      </div>
-    );
+    const chart = ReactFauxDOM.createElement('div');
+
+    d3.select(chart).append('svg')
+      .attr('width',this.props.width)
+      .attr('height', this.props.height)
+      .style('background',this.props.chartBg)
+      .selectAll('rect')
+      .data(this.state.data)
+      .enter().append('rect')
+        .style('fill', this.props.barColor)
+        .attr('width', this.props.barWidth)
+        .attr('height', (d) => {
+          return d;
+        })
+        .attr('x', (d, i) => {
+          return i * (this.props.barWidth + this.props.barOffset);
+        })
+        .attr('y', (d) => {
+          return this.props.height - d;
+        })
+    return chart.toReact();
   }
 }
 
